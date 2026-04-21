afd0115cd4d3717b8c56350afba0371b31078be4
shayan <shayantr2@gmail.com>
2026-04-21 16:04:23 +0330

    [BUG_FIXED] adminserializer class refactored and debuged

--- auth/api/login/__init__.py
+++ auth/api/login/__init__.py
@@ -2,13 +2,12 @@ from rest_framework import status
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from auth.api.login.serializers import AdminSerializer
-from core.models import User
 
 
 class AdminUserLoginAPIView(CreateAPIView):
     permission_classes = [AllowAny]
     serializer_class = AdminSerializer
-    queryset = User.objects.all()
--- auth/api/login/serializers.py
+++ auth/api/login/serializers.py
@@ -4,28 +4,28 @@ from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class AdminSerializer(serializers.Serializer):
-    phone = serializers.CharField()
+    phone = serializers.CharField(write_only=True)
     password = serializers.CharField(write_only=True)
+    user = serializers.HiddenField(default=None)
+    access = serializers.CharField(read_only=True)
+    refresh = serializers.CharField(read_only=True)
 
     def validate(self, attrs):
         user = authenticate(
-            phone = attrs['phone'],
-            password = attrs['password']
+            phone=attrs['phone'],
+            password=attrs['password']
         )
         if user is None:
             raise serializers.ValidationError('Invalid credentials')
-        if user.role != 2:
-            raise serializers.ValidationError('unauthorized user')
         attrs['user'] = user
         return attrs
 
-    def _get_tokens(self, user):
-        refresh = RefreshToken.for_user(user)
+    def _get_tokens(self):
+        refresh = RefreshToken.for_user(self.validated_data.get('user'))
         return {
             'access': str(refresh.access_token),
-            'refresh': str(refresh)
+            'refresh': str(refresh),
         }
 
     def create(self, validated_data):
-        user = validated_data['user']
-        return self._get_tokens(user)
+        return self._get_tokens()
