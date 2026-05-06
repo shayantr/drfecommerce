e8c9b420dc343fd60647fb78d24213a7fda6e59d
shayan <shayantr2@gmail.com>
2026-05-06 13:58:01 +0330

    [bugfix] environ path

--- app/settings.py
+++ app/settings.py
@@ -11,14 +11,19 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import datetime
 import logging
+import os
 from pathlib import Path
 import environ
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
 env = environ.Env(
     DEBUG=(bool, False)
 )
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
