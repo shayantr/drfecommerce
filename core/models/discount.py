1b06991685b3a73daeef6f4f505caa119535459d
shayan <shayantr2@gmail.com>
2026-05-24 13:42:04 +0330

    [Refactor] Simplified discount model

--- core/models/discount.py
+++ core/models/discount.py
@@ -28,45 +28,45 @@ class Discount(BaseModel):
 
     usage_limit = models.PositiveIntegerField(null=True, blank=True)
     used_count = models.PositiveIntegerField(default=0)
-
-    users = models.ManyToManyField(
-        to=User,
-        through= 'DiscountUser',
-        related_name='users',
-        blank=True,
-        help_text="If empty, available for all users"
-    )
-
     is_active = models.BooleanField(default=True)
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
 
-    products = models.ManyToManyField(
-        to='core.Product',
-        through= 'DiscountProduct',
-        related_name='products',
-        blank=True,
-        help_text="If empty, available for all products"
-    )
-    categories = models.ManyToManyField(
-        to='core.Category',
-        through='DiscountCategory',
-        related_name='categories',
-        blank=True,
-        help_text="If empty, available for all categories"
-    )
-    exclude_products = models.ManyToManyField(
-        to='core.Product',
-        through='ExcludeDiscountProduct',
-        related_name='exclude_products',
-        blank=True,
-        help_text="If empty, available for all excluded products"
-    )
+    class Meta:
+        db_table='discount'
+
+    # users = models.ManyToManyField(
+    #     to=User,
+    #     through= 'DiscountUser',
+    #     related_name='users',
+    #     blank=True,
+    #     help_text="If empty, available for all users"
+    # )
+
+    # products = models.ManyToManyField(
+    #     to='core.Product',
+    #     through= 'DiscountProduct',
+    #     related_name='products',
+    #     blank=True,
+    #     help_text="If empty, available for all products"
+    # )
+    # categories = models.ManyToManyField(
+    #     to='core.Category',
+    #     through='DiscountCategory',
+    #     related_name='categories',
+    #     blank=True,
+    #     help_text="If empty, available for all categories"
+    # )
+    # exclude_products = models.ManyToManyField(
+    #     to='core.Product',
+    #     through='ExcludeDiscountProduct',
+    #     related_name='exclude_products',
+    #     blank=True,
+    #     help_text="If empty, available for all excluded products"
+    # )
 
     def is_valid(self, user=None, order_amount=None):
         now = timezone.now()
-        if self.users.exists() and user not in self.users.all():
-            return False, 'not_allowed'
         if not self.is_active:
             return False, "inactive discount"
         if self.start_date and now < self.start_date:
@@ -89,31 +89,31 @@ class Discount(BaseModel):
             discount = self.value
         return discount
 
-class DiscountUser(models.Model):
-    user_id = models.ForeignKey('User', on_delete=models.CASCADE)
-    discount_id = models.ForeignKey('Discount', on_delete=models.CASCADE)
-
-    class Meta:
-        unique_together = ('user_id', 'discount_id')
-        db_table = 'discount_users'
-
-class DiscountProduct(models.Model):
-    product_id = models.ForeignKey('Product', on_delete=models.CASCADE)
-    discount_id = models.ForeignKey('Discount', on_delete=models.CASCADE)
-
-    class Meta:
-        unique_together = ('product_id', 'discount_id')
-        db_table = 'discount_products'
-class DiscountCategory(models.Model):
-    category_id = models.ForeignKey('Category', on_delete=models.CASCADE)
-    discount_id = models.ForeignKey('Discount', on_delete=models.CASCADE)
-
-    class Meta:
-        unique_together = ('category_id', 'discount_id')
-        db_table = 'discount_categories'
-class ExcludeDiscountProduct(models.Model):
-    product_id = models.ForeignKey('Product', on_delete=models.CASCADE)
-    discount_id = models.ForeignKey('Discount', on_delete=models.CASCADE)
-    class Meta:
-        unique_together = ('product_id', 'discount_id')
-        db_table = 'exclude_discount_products'
+# class DiscountUser(models.Model):
+#     user_id = models.ForeignKey('User', on_delete=models.CASCADE)
+#     discount_id = models.ForeignKey('Discount', on_delete=models.CASCADE)
+#
+#     class Meta:
+#         unique_together = ('user_id', 'discount_id')
+#         db_table = 'discount_users'
+#
+# class DiscountProduct(models.Model):
+#     product_id = models.ForeignKey('Product', on_delete=models.CASCADE)
+#     discount_id = models.ForeignKey('Discount', on_delete=models.CASCADE)
+#
+#     class Meta:
+#         unique_together = ('product_id', 'discount_id')
+#         db_table = 'discount_products'
+# class DiscountCategory(models.Model):
+#     category_id = models.ForeignKey('Category', on_delete=models.CASCADE)
+#     discount_id = models.ForeignKey('Discount', on_delete=models.CASCADE)
+#
+#     class Meta:
+#         unique_together = ('category_id', 'discount_id')
+#         db_table = 'discount_categories'
+# class ExcludeDiscountProduct(models.Model):
+#     product_id = models.ForeignKey('Product', on_delete=models.CASCADE)
+#     discount_id = models.ForeignKey('Discount', on_delete=models.CASCADE)
+#     class Meta:
+#         unique_together = ('product_id', 'discount_id')
+#         db_table = 'exclude_discount_products'
