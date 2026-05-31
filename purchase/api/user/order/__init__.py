718d84d344a115535dfef666b5266e1e2d352ca4
shayan <shayantr2@gmail.com>
2026-05-31 19:53:24 +0330

    [REFACTOR] renamed order serializers

--- purchase/api/user/order/__init__.py
+++ purchase/api/user/order/__init__.py
@@ -2,19 +2,19 @@ from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets, mixins
 from core.models import UserOrder
 from core.utils.permissions import AuthenticatedUserViewSet
-from purchase.api.user.order.serializers import UserOrderSerializer, UserOrderDetailSerializer
+from purchase.api.user.order.serializers import AddOrderSerializer, UserOrderDetailSerializer
 
 @extend_schema(
     tags=['order'],
 )
 class UserOrderViewSet(AuthenticatedUserViewSet, viewsets.GenericViewSet, mixins.CreateModelMixin, mixins.ListModelMixin, mixins.RetrieveModelMixin):
     queryset = UserOrder.objects.all()
-    serializer_class = UserOrderSerializer
+    serializer_class = AddOrderSerializer
 
     def get_serializer_class(self):
         if self.action == 'retrieve':
             return UserOrderDetailSerializer
-        return UserOrderSerializer
+        return AddOrderSerializer
 
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user)
