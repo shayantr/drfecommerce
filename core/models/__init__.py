a0c6a5e10a28d8cd1ee4abdfb29ec6418cf35ce8
shayan <shayantr2@gmail.com>
2026-05-31 19:52:49 +0330

    [REFACTOR] added signals to __init__ file

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,7 +1,7 @@
 from .user import User, UserAddress
 from .cart import UserCart, Cart
-from .order import Order, UserOrder
-from .payment import Payment
+from .order import Order, UserOrder, create_order_task
+from .payment import Payment, create_payment_task
 from .discount import Discount
 from .product import Product, ProductImage
 from .category import Category, CategoryProduct
