2616914015801886a58de2904d7f1477064b14b5
shayan <shayantr2@gmail.com>
2026-05-31 19:57:29 +0330

    [REFACTOR] Add to order, functionality moved to userorder model, renamed order serializers

--- core/models/order.py
+++ core/models/order.py
@@ -1,4 +1,5 @@
 from django.db import models, transaction
+from django.db.models import F
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -24,10 +25,55 @@ class UserOrder(BaseModel):
     discount_amount = models.IntegerField(blank=True, null=True, default=0)
     final_amount = models.IntegerField(blank=True, null=True)
     address = models.ForeignKey('UserAddress', on_delete=models.CASCADE, related_name='user_orders')
-    discount = models.ForeignKey('Discount', on_delete=models.CASCADE, related_name='user_orders', blank=True, null=True)
+    discount = models.ForeignKey('Discount', on_delete=models.CASCADE, related_name='user_orders', blank=True,
+                                 null=True)
 
     def get_final_amount(self):
         return self.total_amount - self.discount_amount
+
+    def save_cart_to_order(self, user_cart):
+        from core.models import Discount, Product
+        total = 0
+        discount = None
+        orders = []
+        for item in user_cart.items.all():
+            # update product quantity
+            Product.objects.filter(
+                pk=item.product_id
+            ).update(
+                quantity=F('quantity') - item.quantity
+            )
+            if item.product.sale_price:
+                orders.append(
+                    Order(
+                        user_order=self,
+                        product=item.product,
+                        quantity=item.quantity,
+                        price=item.product.sale_price
+                    )
+                )
+                total += item.product.sale_price * item.quantity
+            else:
+                orders.append(
+                    Order(
+                        user_order=self,
+                        product=item.product,
+                        quantity=item.quantity,
+                        price=item.product.price
+                    )
+                )
+                total += item.product.price * item.quantity
+        Order.objects.bulk_create(orders)
+        if user_cart.discount_code:
+            try:
+                discount = Discount.objects.get(code=user_cart.discount_code)
+            except Discount.DoesNotExist:
+                raise ValueError('Discount not found')
+            discount.validate(total)
+
+        # user_cart.delete()
+        return total, discount
+
 
 @receiver(post_save, sender=UserOrder)
 def create_order_task(sender, instance, created, **kwargs):
@@ -47,5 +93,3 @@ class Order(BaseModel):
     price = models.IntegerField(blank=True, null=True)
     quantity = models.IntegerField(blank=True, null=True)
     user_order = models.ForeignKey('UserOrder', on_delete=models.CASCADE, related_name='orders')
-
-
--- purchase/api/user/order/serializers.py
+++ purchase/api/user/order/serializers.py
@@ -1,16 +1,16 @@
 from django.db import transaction
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
+from django.core.exceptions import ValidationError as DJValidationError
 
 from core.models.discount import DiscountUsage
 from core.utils.get_client_ip import get_client_ip
-from core.models import Order, Cart, UserOrder, UserCart, Product, Payment, Discount
-from purchase.service.product_services import DiscountService
+from core.models import Order, Cart, UserOrder, UserCart
 from purchase.service.zarin_gateway import ZarinGatWay
 from user.api.user import AddressSerializer
 
 
-class OrderDetailSerializer(serializers.ModelSerializer):
+class ItemDetailSerializer(serializers.ModelSerializer):
     total_price = serializers.SerializerMethodField()
     product = serializers.CharField(source='product.title', read_only=True)
     class Meta:
@@ -23,7 +23,7 @@ class OrderDetailSerializer(serializers.ModelSerializer):
 
 class UserOrderDetailSerializer(serializers.ModelSerializer):
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
-    items = OrderDetailSerializer(source='orders', many=True, read_only=True)
+    items = ItemDetailSerializer(source='orders', many=True, read_only=True)
     address = AddressSerializer(read_only=True, many=False)
     status = serializers.CharField(source='get_status_display', read_only=True)
 
@@ -33,7 +33,7 @@ class UserOrderDetailSerializer(serializers.ModelSerializer):
 
 
 
-class UserOrderSerializer(serializers.ModelSerializer):
+class AddOrderSerializer(serializers.ModelSerializer):
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
     link = serializers.CharField(read_only=True)
 
@@ -42,93 +42,43 @@ class UserOrderSerializer(serializers.ModelSerializer):
         fields = ['id', 'user', 'status', 'total_amount', 'address', 'link']
         read_only_fields = ('id', 'status', 'total_amount')
 
-    def _buy_product(self, product, quantity):
-        product = Product.objects.select_for_update().get(pk=product.pk)
-        product.quantity -= quantity
-        product.save()
-
     def validate(self, attrs):
         user = attrs.get('user')
         user_cart = UserCart.objects.filter(user=user)
         if not user_cart.exists():
             raise serializers.ValidationError('cart is empty')
-        else:
-            user_cart = user_cart.first()
-        items = Cart.objects.filter(user_cart=user_cart)
-        for item in items:
+        user_cart = user_cart.prefetch_related('items').first()
+        attrs['user_cart'] = user_cart
+        for item in user_cart.items.all():
             if item.product.quantity < item.quantity:
                 raise serializers.ValidationError('quantity is less than product quantity')
             if item.product.is_active == False or item.product.is_deleted == True or item.product.quantity == 0:
                 raise serializers.ValidationError('product is not active or available')
         return attrs
 
-    def _add_to_order(self, user_order):
-        user = self.context['request'].user
-        user_cart = UserCart.objects.get(user=user)
-        items = Cart.objects.filter(user_cart=user_cart)
-        discount = None
-        total = 0
-        for item in items:
-            self._buy_product(item.product, item.quantity)
-            if item.product.sale_price:
-                Order.objects.create(
-                    user_order=user_order,
-                    product=item.product,
-                    quantity=item.quantity,
-                    price=item.product.sale_price
-                )
-                total += item.product.sale_price * item.quantity
-            else:
-                Order.objects.create(
-                    user_order=user_order,
-                    product=item.product,
-                    quantity=item.quantity,
-                    price=item.product.price
-                )
-                total += item.product.price * item.quantity
-        if user_cart.discount_code:
-            try:
-                discount = Discount.objects.get(code=user_cart.discount_code)
-            except Discount.DoesNotExist:
-                raise ValidationError('discount does not exist')
-            DiscountService.validate(
-                discount=discount,
-                total=total,
-            )
-
-        # user_cart.delete()
-        return total, discount
-
     def create(self, validated_data):
         with transaction.atomic():
+            user_cart = validated_data.pop('user_cart')
             user_order = UserOrder.objects.create(**validated_data)
-            user = user_order.user
-            total, discount = self._add_to_order(user_order)
+            try:
+                total, discount = user_order.save_cart_to_order(user_cart)
+            except ValueError as e:
+                raise ValidationError(str(e))
             user_order.discount = discount
             user_order.total_amount = total
             user_order.discount_amount = discount.calculate_discount(total)
             user_order.final_amount = total - user_order.discount_amount
             user_order.save()
             DiscountUsage.objects.create(
-                user=user,
+                user=validated_data['user'],
                 discount=discount,
                 user_order=user_order,
             )
-            gateway = ZarinGatWay(user_order)
+            ip = get_client_ip(self.context.get('request'))
+            gateway = ZarinGatWay(user_order, ip)
             res = gateway.request()
             if not res['success']:
                 raise serializers.ValidationError(res['error'])
-            ip = get_client_ip(self.context.get('request'))
-            Payment.objects.create(
-                user=user,
-                link=gateway.get_link(res['data']['authority']),
-                authority=res['data']['authority'],
-                user_order=user_order,
-                amount=total,
-                gateway=gateway,
-                status=1,
-                ip_address=ip
-            )
 
         return {
             'total_amount': total,
