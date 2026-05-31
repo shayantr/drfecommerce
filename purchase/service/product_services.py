7e5402cb6f64e07e3667140c8cbb46c7a73c4ce1
shayan <shayantr2@gmail.com>
2026-05-31 19:50:42 +0330

    [CLEANUP] moved discount validate to model

--- purchase/service/product_services.py
+++ purchase/service/product_services.py
@@ -1,7 +1,4 @@
-from rest_framework.exceptions import ValidationError
-
-from core.models import Product, Discount
-from core.models.discount import DiscountUsage
+from core.models import Product
 
 
 def restore_product_reservation(item):
@@ -9,17 +6,3 @@ def restore_product_reservation(item):
     product.quantity += item.quantity
     product.save()
 
-class DiscountService:
-    @staticmethod
-    def validate(discount: Discount, total):
-        if not discount.is_valid:
-            raise ValidationError("Invalid discount")
-        if discount.min_order_amount & total < discount.min_order_amount:
-            raise ValidationError('min order amount required')
-
-        usage_count = DiscountUsage.objects.filter(
-            discount=discount,
-        ).count()
-        if discount.usage_limit and usage_count >=discount.usage_limit:
-            raise ValidationError('usage limit reached')
-        return True
